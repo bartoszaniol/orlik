60eaeaf59224ca81d369aba55fb3c5c2f6046176
Bartosz <bartek.104@o2.pl>
2023-09-29 12:28:03 +0200

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { hostname } from 'os';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Test API (Books CRUD)')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
